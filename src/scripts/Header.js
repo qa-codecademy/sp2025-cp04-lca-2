2125729ff92904e1198be9292829b1d84ccdb8b9
aaaa <bojan.20406@gmail.com>
2025-07-03 20:03:53 +0200

    fix

--- src/scripts/Header.js
+++ src/scripts/Header.js
@@ -1,10 +1,24 @@
-const burgerIcon = document.querySelector('#burger-icon')
-const crossIcon = document.querySelector('#cross-icon')
-const sideMenu = document.querySelector('.header_nav_section_mobile_nav')
-
-burgerIcon.addEventListener('click', () => {
-    sideMenu.classList.toggle('open');
-});
-crossIcon.addEventListener('click', () => {
-    sideMenu.classList.toggle('open');
-});
+document.addEventListener("DOMContentLoaded", function () {
+    const burgerIcons = document.querySelectorAll('#burger-icon');
+    const crossIcons = document.querySelectorAll('#cross-icon');
+    const sideMenus = document.querySelectorAll('.header_nav_section_mobile_nav');
+  
+    burgerIcons.forEach((burgerIcon, index) => {
+      const sideMenu = sideMenus[index] || sideMenus[0];
+      if (burgerIcon && sideMenu) {
+        burgerIcon.addEventListener('click', () => {
+          sideMenu.classList.toggle('open');
+        });
+      }
+    });
+  
+    crossIcons.forEach((crossIcon, index) => {
+      const sideMenu = sideMenus[index] || sideMenus[0];
+      if (crossIcon && sideMenu) {
+        crossIcon.addEventListener('click', () => {
+          sideMenu.classList.toggle('open');
+        });
+      }
+    });
+  });
+  
